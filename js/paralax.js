da08f22cd73548f160be7205dfe47a4e6b6cba82
Kanto <kanto72@yandex.ru>
2020-12-08 16:06:28 +0500

    paralaks kill me

--- js/paralax.js
+++ js/paralax.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
 function parallax(distance, itemBg, itemElement=null) {
     window.addEventListener('mousemove', function (e) {
         const x = e.clientX / distance;
-        itemBg.style.backgroundPosition = `-${x * 70}px`;
+        itemBg.style.backgroundPosition = `-${x * 50}px`;
         if (itemElement)
-            itemElement.style.transform = `translateX(${x * 70}px)`;
+            itemElement.style.transform = `translateX(${x * 50}px)`;
     });
 }
 
@@ -45,6 +45,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (windowWidth > 992) {
         parallax3(windowWidth, headerBg, headerItem);
+    }
+});
+function parallax3(distance, itemBg, itemElement=null) {
+    window.addEventListener('mousemove', function (e) {
+        const x = e.clientX / distance;
+        itemBg.style.backgroundPosition = `-${x * -50}px`;
+        if (itemElement)
+            itemElement.style.transform = `translateX(${x * -50}px)`;
+    });
+}
+
+
+//paralax number four --run
+document.addEventListener('DOMContentLoaded', function () {
+    const windowWidth = window.innerWidth;
+    const headerBg = document.querySelector('.run');
+    const headerItem4 = document.querySelector('.run-block__fon');
+
+    if (windowWidth > 992) {
+        parallax3(windowWidth, headerBg, headerItem4);
+    }
+});
+function parallax3(distance, itemBg, itemElement=null) {
+    window.addEventListener('mousemove', function (e) {
+        const x = e.clientX / distance;
+        itemBg.style.backgroundPosition = `-${x * +70}px`;
+        if (itemElement)
+            itemElement.style.transform = `translateX(${x * +70}px)`;
+    });
+}
+
+//paralax number five --run
+document.addEventListener('DOMContentLoaded', function () {
+    const windowWidth = window.innerWidth;
+    const headerBg = document.querySelector('.run');
+    const headerItem5 = document.querySelector('.run-block__player');
+
+    if (windowWidth > 992) {
+        parallax3(windowWidth, headerBg, headerItem5);
     }
 });
 function parallax3(distance, itemBg, itemElement=null) {
