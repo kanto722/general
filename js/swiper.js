26351ccea2b92a77121498c8b09c70e9157af10a
Kanto <kanto72@yandex.ru>
2020-12-11 15:39:50 +0500

    go go go

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,13 @@
 const slider = document.querySelector('.swiper-container');
+const slider2 = document.querySelector('.swiper-container2');
+
 
 let mySwiper = new Swiper(slider, {
     slidesPerView: 3,
     spaceBetween: 20,
-    // autoplay: {
-    //   delay: 2000,
-    // },
+    autoplay: {
+      delay: 2000,
+    },
     
     navigation: {
         nextEl: '.swiper-button-next',
@@ -42,3 +44,48 @@ let mySwiper = new Swiper(slider, {
       },
     }
   })
+
+  let mySwiper2 = new Swiper(slider2, {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // autoplay: {
+    //   delay: 2000,
+    // },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      loop: true,
+    lazy: {
+      loadPrevNext: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      670: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1300: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    }
+  })
+
+
