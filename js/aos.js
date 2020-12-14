f3212c2f9114e07fc35562d9eceaee621e8ddd94
Kanto <kanto72@yandex.ru>
2020-12-14 15:28:48 +0500

    googlepsges

--- js/aos.js
+++ js/aos.js
@@ -1,6 +1,6 @@
 AOS.init({
     // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    disable: true, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
     initClassName: 'aos-init', // class applied after initialization
     animatedClassName: 'aos-animate', // class applied on animation
@@ -11,7 +11,7 @@ AOS.init({
     
   
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 190, // offset (in px) from the original trigger point
+    offset: 110, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,4 @@
 const slider = document.querySelector('.swiper-container');
-const slider2 = document.querySelector('.swiper-container2');
 
 let mySwiper = new Swiper(slider, {
     slidesPerView: 3,
@@ -48,48 +47,3 @@ let mySwiper = new Swiper(slider, {
       },
     }
   })
-
-  let mySwiper2 = new Swiper(slider2, {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    autoplay: {
-      delay: 2000,
-    },
-    lazy: true,
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      loop: true,
-    lazy: {
-      loadPrevNext: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 10
-      },
-      670: {
-        slidesPerView: 2,
-        spaceBetween: 10
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 10,
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 10,
-      },
-      1300: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-    }
-  })
-
