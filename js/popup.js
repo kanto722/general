c3f5432bb7ca36c08ff4e8b39b1f428fb10f8e6c
Kanto <kanto72@yandex.ru>
2020-12-15 10:15:21 +0500

    project

--- js/popup.js
+++ js/popup.js
@@ -125,3 +125,5 @@ function closeEmail(){
 
 };
 // email
+
+
---
+++ js/project.js
@@ -0,0 +1,7 @@
+function openContent(){
+    $('.gallery-content-project').addClass('gallery-content-project_invite');
+};
+
+function closeContent(){
+    $('.gallery-content-project').removeClass('gallery-content-project_invite');
+};
